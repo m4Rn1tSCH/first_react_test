2f3475be3bc9a39f50af6bd734e8064a1f469394
Bill_Jaenke <bjaenke2017@student.hult.edu>
2021-02-23 18:54:40 -0500

    -initial attempt with React

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'React';
+  const welcome = {
+    greeting: 'Dear',
+    title: 'Sir',
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {/*PLAYGROUND AREA*/}
           Edit <code>src/App.js</code> and save to reload.
+          <div>
+            <h1>{welcome.greeting} {welcome.title}</h1>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+          </div>
         </p>
         <a
           className="App-link"
