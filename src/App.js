376011cbcf4ead18e2359d1e55f7fcbe761a35b6
Bill_Jaenke <bjaenke2017@student.hult.edu>
2021-04-15 10:46:38 -0400

    -changed to reusable components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import React from "react";
 import './App.css';
+import { Button } from '@material-ui/core';
 
-
+//Documentation - Javascript
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript
+//Documentation - React
+// https://reactjs.org/docs/hello-world.html
 
 /*when functions are defined here; they need to be used in the return section*/
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
 
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
 // App Section
 const App = () => {
 //inside the app component
+
   const stories = [
   {
   title: 'React',
@@ -29,7 +44,8 @@ const App = () => {
   ];
   /*The search term is needed in App to filter the list before passing it to the List
    *component as props
-   *lift state up from Search to App component to share the state with more components*/
+   *lift state up from Search to App component to share the state with more components
+   * Hooks dont re-initialize again, it takes the recent value (current state) */
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleSearch = event => {
@@ -44,11 +60,17 @@ const App = () => {
 
   return (
     <div>
-      <h1>My Hacker Stories</h1>
+        <head>
+            <meta
+                name="viewport"
+                content="minimum-scale=1, initial-scale=1, width=device-width"
+            />
+        </head>
+        <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch}/>
+        <Search search={searchTerm} onSearch={handleSearch}/>
 
-      <hr />
+        <hr />
 
       <List list={searchedStories} />
     </div>
@@ -61,40 +83,49 @@ const App = () => {
 * the second value is a function to update this state (setSearchTerm)*/
 //FUNCTION DEFINITION used in the app part
 //props are only passed downwards
-const Search = props => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
 
-    props.onSearch(event);
-  };
-  return (
-    <div>
-    <label htmlFor="search">Search: </label>
+/* Component Composition
+* instead of label prop, use children to have access to InputWithLabel component
+* that can render everything that is passed down */
+const InputWithLabel = ({
+    id,
+    value,
+    type = 'text',
+    onInputChange,
+    children,
+    }) => (
+    <>
+        <label htmlFor={id}>{children}</label>
+        &nbsp;
+
     <input
-        id="search"
-        type="text"
-        value={props.search}
-        onChange={props.onSearch} />
-    </div>
-  );
-};
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+    />
+    </>
+    );
+
 /*Function for a list iteration
 * variable stories is called stories for the  app component
 * access a list from the props object without having a global variable outside all functions*/
-const List = props =>
-  props.list.map(item => (
-    <div key={item.objectID}>
-    <span>
-    <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    </div>
-  ));
+  const List = ({ list }) =>
+    list.map(item => <Item key={item.objectID} item={item} />);
 
-/*Curly brackets cna be left out when the body does not perform tasks but only displays
+  const Item = ({ item }) => (
+    <div>
+            <span>
+            <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+    </div>
+    );
+
+
+/*Curly brackets can be left out when the body does not perform tasks but only displays
 // with block body
 count => {
 // perform any task in between
@@ -104,6 +135,5 @@ return count + 1;
 count =>
 count + 1;
 */
-
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,7 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('true to be true', () => {
+  expect(true).toBe(true);
+});
